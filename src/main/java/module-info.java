76f10daf3ae40cd7353917a0ff764d9a08ff1e99
Imikaki <hoanghoangminhtu1708@gmail.com>
2022-09-23 10:31:36 +0700

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.bomberman {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.bomberman to javafx.fxml;
+    exports com.example.bomberman;
+}
