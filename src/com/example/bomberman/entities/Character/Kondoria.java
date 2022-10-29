8d0e28a6de9c12ef8deb061813495c85e7ec8c3b
DaoVanThanh <thanhap03@gmail.com>
2022-10-30 01:07:57 +0700

    small change

--- src/com/example/bomberman/entities/Character/Kondoria.java
+++ src/com/example/bomberman/entities/Character/Kondoria.java
@@ -1,6 +1,7 @@
 package com.example.bomberman.entities.Character;
 
 import com.example.bomberman.entities.Entity;
+import com.example.bomberman.graphics.Sprite;
 import com.example.bomberman.system.Direction;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -8,6 +9,10 @@ import javafx.scene.image.Image;
 public class Kondoria extends Enemies {
     public Kondoria(int x, int y, Image img) {
         super(x, y, img);
+        enemyLeft = new Image[]{Sprite.kondoria_left1.getFxImage(),Sprite.kondoria_left2.getFxImage(),Sprite.kondoria_left3.getFxImage()};
+        enemyRight = new Image[]{Sprite.kondoria_right1.getFxImage(),Sprite.kondoria_right2.getFxImage(),Sprite.kondoria_right3.getFxImage()};
+        enemyDie = Sprite.kondoria_dead.getFxImage();
+
     }
 
     @Override
--- src/com/example/bomberman/entities/Character/Minvo.java
+++ src/com/example/bomberman/entities/Character/Minvo.java
@@ -1,6 +1,7 @@
 package com.example.bomberman.entities.Character;
 
 import com.example.bomberman.entities.Entity;
+import com.example.bomberman.graphics.Sprite;
 import com.example.bomberman.system.Direction;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -8,6 +9,9 @@ import javafx.scene.image.Image;
 public class Minvo extends Enemies {
     public Minvo(int x, int y, Image img) {
         super(x, y, img);
+        enemyLeft = new Image[]{Sprite.minvo_left1.getFxImage(),Sprite.minvo_left2.getFxImage(),Sprite.minvo_left3.getFxImage()};
+        enemyRight = new Image[] {Sprite.minvo_right1.getFxImage(),Sprite.minvo_right2.getFxImage(),Sprite.minvo_right3.getFxImage()};
+        enemyDie = Sprite.minvo_dead.getFxImage();
     }
 
     @Override
